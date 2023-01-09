13aebad8933753c8fb24b264efbd26ec652ecab2
Kun Lu <95984858+gelileo@users.noreply.github.com>
2023-01-09 16:27:05 +0000

    a test

--- Main.java
+++ Main.java
@@ -1,19 +1,21 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
+    Test test = new Test();
+    System.out.println(test.fibonacci(5));
   }
+}
 
+class Test {
   int fibonacci(int i) {
-  if (i < 1) {
-    return 0;
-  } else if (i == 2) {
-    return 2;
-  } else if (i == 3) {
-    return 3;
-  } else {
-  return fibonacci(i-1) + fibonacci(i-2)
+    if (i < 1) {
+      return 0;
+    } else if (i == 2) {
+      return 2;
+    } else if (i == 3) {
+      return 3;
+    } else {
+      return fibonacci(i - 1) + fibonacci(i - 2);
+    }
   }
-
 }
-}
-
